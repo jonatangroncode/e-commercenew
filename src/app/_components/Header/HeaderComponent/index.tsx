5458461ef3c6ee8e285a93fb2f7c06df585398cc
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-01-31 20:30:05 +0100

    footer

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 
 import React from 'react'
 import Link from 'next/link'
@@ -9,17 +9,24 @@ import { Gutter } from '../../Gutter'
 import classes from './index.module.scss'
 import Image from 'next/image'
 import { HeaderNav } from '../Nav'
-import MobileNav from '../MobileNav'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+
   return (
-    <nav >
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes
+        (pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
       <Gutter className={classes.wrap}>
         <Link href={'/'}>
-          <Image src="/test.svg" alt="logo" width={170} height={50}/>
-          
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
         </Link>
-        <HeaderNav header={header}/>
+        <HeaderNav header={header} />
        
       </Gutter>
     </nav>
